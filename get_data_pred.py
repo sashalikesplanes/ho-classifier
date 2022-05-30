7dbc31e6344c3d3e9f284ca57594814345f39761
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-05-30 22:16:32 +0200

    created files for testing varying preview

--- get_data_pred.py
+++ get_data_pred.py
@@ -2,6 +2,78 @@ import numpy as np
 from scipy import io
 import random
 from sklearn.preprocessing import StandardScaler
+from pathlib import Path
+
+
+# TO ANY FUTURE READER:
+#   This file was made extremely quick and dirty right before the deadline
+#   I severly apoligize for any pain you may have experienced while reading this code
+#   Please do not judge me too harshly
+#   - sasha
+def get_var_prev_data(file):
+    
+    data_dict = io.loadmat(file, simplify_cells=True)['ed']
+    desired_conditions = ['PRL', 'PRM', 'PRH']
+    run_indices = [0]
+    subj_indices = [0, 1, 2, 3, 4]
+    n_subjects = len(subj_indices)
+    n_runs = len(run_indices)
+    n_conditions = len(desired_conditions)
+    variables=['e', 'u', 'x', 'dedt', 'dudt', 'dxdt']
+    n_variables = len(variables)
+    time_window=150
+    total_time_steps=12000
+    sample_freq=50
+    time_steps_between_samples=75
+    data_freq=100
+    n_samples = round((total_time_steps - time_window) / time_steps_between_samples *
+                            n_conditions * n_subjects * n_runs)
+    print(data_dict['PRL']['e'])
+    all_vars_per_cond = []
+    for cond in desired_conditions:
+        all_vars = np.empty(
+            (len(variables), total_time_steps, (n_runs), n_subjects))
+        for i, variable in enumerate(variables):
+            try:
+                all_vars[i] = np.array(data_dict[cond][variable]).reshape((12000, 1, 5))
+            # Will fail if the variable is one of the derivatives as those are not in raw data
+            # so we calculate derivative manually
+            except KeyError:
+                if variable == 'dedt':
+                    all_vars[i] = np.gradient(
+                        data_dict[cond]['e'].reshape((12000, 1, 5)), 1 / data_freq, axis=0)
+                if variable == 'dudt':
+                    all_vars[i] = np.gradient(
+                        data_dict[cond]['u'].reshape((12000, 1, 5)), 1 / data_freq, axis=0)
+                if variable == 'dxdt':
+                    all_vars[i] = np.gradient(
+                        data_dict[cond]['x'].reshape((12000, 1, 5)), 1 / data_freq, axis=0)
+            var_for_scaling = all_vars[i].reshape(
+                (total_time_steps, n_runs * n_subjects))
+            var_scaled = StandardScaler(
+                copy=False).fit_transform(var_for_scaling)
+            all_vars[i] = var_scaled.reshape(
+                (total_time_steps, n_runs, n_subjects))
+        all_vars_per_cond.append(all_vars)
+    # condition x variable x time x run x subject
+    data_array = np.stack(all_vars_per_cond)
+    del all_vars_per_cond
+    
+
+    X = np.empty((n_samples, n_variables, time_window))
+    
+    sample_index = 0
+    for cond_index in range(n_conditions):
+        for subj_index in subj_indices:
+            for run_index in run_indices:
+                for t in range(0, total_time_steps - time_window, time_steps_between_samples):
+                    X[sample_index] = data_array[cond_index, :,
+                                                 t:t + time_window, run_index, subj_index]
+                    sample_index += 1
+
+    del data_array
+    # Remove samples to reduce the final sample rate
+    return X[:, :, ::int(data_freq / sample_freq)]
 
 def get_fofu_data(valid_pct=0.2,
                   random_labels=False,
@@ -109,51 +181,6 @@ def get_fofu_data(valid_pct=0.2,
     # Remove samples to reduce the final sample rate
     return X[:, :, ::int(data_freq / sample_freq)], Y, splits
 
-
-def get_fofu_data_experimnetal(valid_pct=0.2,
-                               data_file='expdata_SI.mat',
-                               variables=['e', 'u', 'x',
-                                          'dedt', 'dudt', 'dxdt'],
-                               time_window=150,
-                               n_time_steps=12000,
-                               time_steps_between_samples=75,
-                               seconds_per_step=0.01,
-                               subj_indices=[0, 1, 2, 3, 4, 5, 6, 7, 8],
-                               run_indices=[0, 1, 2, 3, 4],
-                               desired_conditions=['CL', 'CM', 'CH',
-                                                   'PL', 'PM', 'PH', 'PRL', 'PRM', 'PRH'],
-                               display_types=[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
-                               n_displays=3, n_conditions_per_display=3):
-    """
-    This function exists to load the data from the last VdEl experiment (which contained 9 subjects
-    each in 9 conditions performing 5 runs per condition) stored as a matlab struct
-    In the form convenient for the usage of classifying which display type was used
-    by a machine learning algorithm
-    """
-    n_subjects = len(subj_indices)
-    n_conditions = len(desired_conditions)
-    n_runs = len(run_indices)
-    # number of variables selected for the run (e, u, x)
-    n_variables = len(variables)
-    n_samples = round((n_time_steps - time_window) / time_steps_between_samples *
-                      n_conditions * n_subjects * n_runs)
-    n_runs_total_per_display = n_subjects * n_runs * n_conditions_per_display
-
-    # Iterate over the 3 display types
-    # Per type, make an array of shape (n_variables, n_time_steps, n_runs_total_per_display)
-    #   also generating the derivative variables
-    # Standardize the array per variable
-    StandardScaler(copy=False)
-    # Transpose the array to (n_runs_total_per_display, n_variables,  n_time_steps)
-    # iterate over runs
-    #   randomly set run to validation with probability valid_pct
-    #   append all samples from the run to X (n_samples, n_variables, time_window)
-    #   append the display type to Y (n_samples,)
-    #   append the current sample index to either validation or train array for splits
-    data_dict = io.loadmat(data_file, simplify_cells=True)['ed']
-    for display_label in range(n_displays):
-        all_runs_per_display = np.empty(
-            n_variables, n_time_steps, n_runs_total_per_display)
-        # for condition
-
-
+if __name__ == "__main__":
+    file = Path('expdata_var_prev.mat')
+    print(get_var_prev_data(file))
